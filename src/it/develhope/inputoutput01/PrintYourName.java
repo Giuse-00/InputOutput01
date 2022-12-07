e8565cf12664d1dc8827e59bd1f69158ea07b9ef
Giuseppe Iacono <giuseiacono10@gmail.com>
2022-12-07 17:42:50 +0100

    Iacono esercizio Input and Output 01

---
+++ src/it/develhope/inputoutput01/PrintYourName.java
@@ -0,0 +1,11 @@
+package it.develhope.inputoutput01;
+
+public class PrintYourName {
+
+    public static void main(String[] args) {
+
+        String name = "Giuseppe";
+
+        System.out.println("My name is " + name);
+    }
+}
